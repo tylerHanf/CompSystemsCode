752159b7d715d65d38330dada05d1cab237809b7
tylerHanf <th8246@rit.edu>
2020-04-23 20:39:37 -0400

    Finished problem 57

--- Chap2/hw/p57.c
+++ Chap2/hw/p57.c
@@ -1,13 +1,45 @@
+/**
+ * Takes in a short, long, and double as command args
+ * and prints hex value of the bytes
+ **/
+
 #include <stdio.h>
-void show_short(int s) {
-	for (int i=0; i<4; i++){
-		printf("%02x", ((unsigned char*)&s)[i]);
+#include <stdlib.h>
+
+#define LEN_SHORT sizeof(short int)
+#define LEN_LONG sizeof(long int)
+#define LEN_DOUBLE sizeof(double)
+
+void show_bytes(unsigned char* data, int len) {
+	printf("Hex:");
+	for (int i=0; i<len; i++) {
+		printf(" %.2x", data[i]);
 	}
 	printf("\n");
 }
 
+void show_short(short int s) {
+	show_bytes((unsigned char*) &s, LEN_SHORT);
+}
+
+void show_long(long int l) {
+	show_bytes((unsigned char*) &l, LEN_LONG);
+}
+
+void show_double(double d) {
+	show_bytes((unsigned char*) &d, LEN_DOUBLE);
+}
+
 int main(int argc, const char* argv[]) {
-	int first = 0x01234567;
-	show_short(first);
+	short int s = atoi(argv[1]);
+	long int l = atoi(argv[2]);
+	double d = atof(argv[3]);
+
+	printf("Showing short int:\n");
+	show_short(s);
+	printf("\nShowing long int:\n");
+	show_long(l);
+	printf("\nShowing double:\n");
+	show_double(d);
 	return 0;
 }
