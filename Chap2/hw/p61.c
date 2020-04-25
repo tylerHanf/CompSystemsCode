30d689b06987cae28680c50af6c5ca695410e10b
tylerHanf <th8246@rit.edu>
2020-04-25 12:51:28 -0400

    Finished problem 61

--- Chap2/hw/p61.c
+++ Chap2/hw/p61.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 
-int bits_are_one(int x) {
-	return x & 0xFF
+#define INT_SIZE (sizeof(int)<<3)
+#define BYTE_SIZE 8
+
+/**
+ * Returns 1 if at least one bit is one
+ **/
+int bit_is_one(int x) {
+	return x && 1;
 }
 
-int main(int argc, const char* argv[]) {
+/**
+ * Returns 1 if at least one bit is zero
+ **/
+int bit_is_zero(int x) {
+	int mask = ~0;
+	return (x ^ mask) && 1;
+}
 
+/**
+ * Returns 1 if at least one bit in the least
+ * significant byte is 1
+ **/
+int one_in_small_byte(int x) {
+	return (x << (INT_SIZE-BYTE_SIZE)) && 1;
+}
+
+/**
+ * Returns 1 if at least one bit in the most
+ * significant byte is 0
+ **/
+int zero_in_big_byte(int x) {
+	int shift = INT_SIZE-BYTE_SIZE;
+	int mask = 0xFFFFFF;
+	int no_zero = 0xFFFFFFFF;
+	x |= (x ^ mask); 		
+	x ^= no_zero;
+	return x && 1;
 }
