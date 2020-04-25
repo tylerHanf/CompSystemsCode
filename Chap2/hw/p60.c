7ed97e01e8c2d2cf5b6149829dd361f73d74539c
tylerHanf <tyler.hanf21@gmail.com>
2020-04-24 21:59:29 -0400

    Finished problem 60

--- Chap2/hw/p60.c
+++ Chap2/hw/p60.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-#define BYTE_SIZE 4
-unsigned replace_byte(unsigned x, int i, unsigned y) {
+/**
+ * Swaps a byte in a w-bit word at position i where i ranges from
+ * 0 to w/8-1, with a given char y.
+ **/
 
-	unsigned mask = ~0x8;
-	mask = mask << i*BYTE_SIZE;
-	printf("%d\n", sizeof(y));
-	printf("%x\n", x & mask);
+#include <stdio.h>
+#define BYTE_SIZE 8
+
+unsigned replace_byte(unsigned x, int i, unsigned char y) {
+	unsigned mask = 0xFF;
+	mask = mask << (i*BYTE_SIZE);
+	x = x & ~mask;
+	return x | (y << i*BYTE_SIZE);
 }
 
 int main(int argc, const char* argv[]) {
-	replace_byte(0x12345678, 4, 0xAB);
+	printf("0x%x\n", replace_byte(0x12345678, 0, 0xAB));
 	return 0;
 }
